7916ba5f3b607ebf47394b1c9215a31c7171cd49
Jose Gutierrez <josegtzr99@gmail.com>
2019-10-18 21:30:04 -0400

    SALIO EL TOKEEEENNNN

---
+++ contacto/appContactSecurity/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace appContactSecurity.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<appContactSecurity.Models.Contact> Contacts { get; set; }
+    }
+}
